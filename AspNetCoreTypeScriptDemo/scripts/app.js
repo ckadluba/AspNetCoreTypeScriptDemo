e31172e5c0b43ee44fd222cfed38e861c9683ab8
Christian Kadluba <10721825+ckadluba@users.noreply.github.com>
2020-02-26 02:19:57 +0100

    Add project files.

---
+++ AspNetCoreTypeScriptDemo/scripts/app.js
@@ -0,0 +1,6 @@
+function sayHello() {
+    const compiler = document.getElementById("compiler").value;
+    const framework = document.getElementById("framework").value;
+    return `Hello from ${compiler} and ${framework} 😊!`;
+}
+//# sourceMappingURL=app.js.map
